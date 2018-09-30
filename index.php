37d3b8718cf1284827e3f0e903fe81bb87b226b8
judy_hartman <jhartma4@lesley.edu>
2018-09-30 14:12:23 -0400

    Removed comments

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
         <?php include 'nav.php' ?>
         <?php
             function getPostTitlesFromDatabase() {
-                // TODO in Module 4
-                // get this data from a database instead of hardcoding it
                 $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
                 return $postTitles;
             }
