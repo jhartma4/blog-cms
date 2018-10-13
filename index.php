03eee00e9c28b5d763ba637672768fd415ee72c9
judy_hartman <jhartma4@lesley.edu>
2018-10-13 16:53:29 -0400

    ndex edits

--- index.php
+++ index.php
@@ -4,27 +4,27 @@
         <title>My New Blog</title>
         <link rel="stylesheet" type="text/css" href="format-new.css">
     </head>
-<body>
+    <body>
 $conn;
-    <?php include 'header.php' ?>
-    <?php include 'nav.php' ?>
-    <?php
-        function getPostTitlesFromDatabase() {
-            $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
-            return $postTitles;
-           }
-    ?>
+        <?php include 'header.php' ?>
+        <?php include 'nav.php' ?>
+        <?php
+            function getPostTitlesFromDatabase() {
+                $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
+                return $postTitles;
+            }
+        ?>
         <main>
             <ul class="blog-list">
-    <?php 
-            $postTitles = getPostTitlesFromDatabase();
+                <?php 
+                    $postTitles = getPostTitlesFromDatabase();
 
-            foreach($postTitles as $postTitle) {
-                echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
-            }
-    ?>
+                    foreach($postTitles as $postTitle) {
+                        echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
+                }
+        ?>
             </ul>
         </main>
-    <?php include 'footer.php' ?>
-</body>
+        <?php include 'footer.php' ?>
+    </body>
 </html>
