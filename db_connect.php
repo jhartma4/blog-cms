1cf1948c8ad2849c16c8cf9a5305f3d5e2565635
judy_hartman <jhartma4@lesley.edu>
2018-10-13 16:54:47 -0400

    space fixed

--- db_connect.php
+++ db_connect.php
@@ -5,6 +5,6 @@ $dbUsername = "root";
 $dbPassword = "root";    
 $dbName = "blog_cms";
 
-$conn = mysqli_connect ($dbServername, $dbUsername, $dbPassword, $dbName);
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 ?>
