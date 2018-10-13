fda21fb07fc286b35cdd13058840b0249dc0b850
judy_hartman <jhartma4@lesley.edu>
2018-10-13 18:41:27 -0400

    php exe settings index update

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include_once 'db_connect.php'; ?>
 
 <!DOCTYPE html>
+<html>
     <head>
         <title>My New Blog</title>
         <link rel="stylesheet" type="text/css" href="format-new.css">
