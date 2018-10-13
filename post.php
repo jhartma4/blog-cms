af07563c1e3dcb1169141f54ddcce78ca9795563
judy_hartman <jhartma4@lesley.edu>
2018-10-13 17:05:49 -0400

    post edits

--- post.php
+++ post.php
@@ -5,17 +5,14 @@
         <main>
             <?php
                 function getPostDetailsFromDatabase() {
-                // Get the post title
-                $postTitle = rawurldecode($_GET["title"]);
+                    $postTitle = rawurldecode($_GET["title"]);
 
-                // Get the post that matches the postTitle
-                include_once 'db_connect.php';
-                $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
-                $result = mysqli_query($conn, $sql);
+                    include_once 'db_connect.php';
+                    $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
+                    $result = mysqli_query($conn, $sql);
 
-                // Get the first row from the result as an associative array
-                $postDetails = mysqli_fetch_assoc($result);
-                return $postDetails;
+                    $postDetails = mysqli_fetch_assoc($result);
+                    return $postDetails;
                 }
         </main>
         <?php include 'footer.php' ?>
