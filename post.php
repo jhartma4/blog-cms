3b84186bdfb1638d61f167066836b9673035d529
judy_hartman <jhartma4@lesley.edu>
2018-10-13 18:50:00 -0400

    Fixe post file yay

--- post.php
+++ post.php
@@ -5,15 +5,26 @@
         <main>
             <?php
                 function getPostDetailsFromDatabase() {
+                    // Get the post title
                     $postTitle = rawurldecode($_GET["title"]);
 
+                    // Get the post that matches the postTitle
                     include_once 'db_connect.php';
                     $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
                     $result = mysqli_query($conn, $sql);
 
+                    // Get the first row from the result as an associative array
                     $postDetails = mysqli_fetch_assoc($result);
                     return $postDetails;
                 }
+                
+                // Post details contains all the data to generate the blog from
+                $postDetails = getPostDetailsFromDatabase();
+            ?>
+            <h2 class="post-title"><?php echo $postDetails["title"]; ?></h2>
+            <div class="post-date"><?php echo $postDetails["date"]; ?></div>
+            <div class="post-content"><?php echo $postDetails["content"]; ?></div>
+            <div class="post-author"><?php echo $postDetails["author"]; ?></div>
         </main>
         <?php include 'footer.php' ?>
     </body>
