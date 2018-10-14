12a4547588afab249b5074b79d03bc5c4599010d
judy_hartman <jhartma4@lesley.edu>
2018-10-14 15:35:26 -0400

    refactored headerphp

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 <header>
-<link rel="stylesheet" type="text/css" href="format-new.css" /> 
+<link rel="stylesheet" type="text/css" href="format-new.css" />
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+         crossorigin="anonymous"></script> 
 	<div id="topbar">
 	 	<div class="logo">
 			My New Blog
--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 <html>
     <head>
         <title>My New Blog</title>
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-         crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="format-new.css">
         <script src="hello.js"></script>
     </head>
