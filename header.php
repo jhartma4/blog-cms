c8ccab8911bc1ff1c26e3ce810d0ac595e271aef
judy_hartman <jhartma4@lesley.edu>
2018-10-16 18:13:12 -0400

    week 5

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <header>
 <link rel="stylesheet" type="text/css" href="format-new.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.1/ace.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
          crossorigin="anonymous"></script> 
